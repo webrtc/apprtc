0e353e732f0ce8c9f414c399ecd26cd80c77b032
Christoffer Jansson <jansson@google.com>
2016-11-02 09:02:05 +0100

    Adding back TLS1.x ciphers to increase compatability

--- src/collider/collider/collider.go
+++ src/collider/collider/collider.go
@@ -57,6 +57,9 @@ func (c *Collider) Run(p int, useTls bool) {
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 			},
 			PreferServerCipherSuites: true,
 		}
