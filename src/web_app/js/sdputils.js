981261e07217fbef8fcb8e3c01fb3d52aa4b2e2f
Tyler Gibson <tyler.gibson@microsoft.com>
2018-01-22 19:29:12 -0800

    Find all matching SDP codecs for defaults

--- src/web_app/js/sdputils.js
+++ src/web_app/js/sdputils.js
@@ -330,9 +330,15 @@ function maybePreferCodec(sdp, type, dir, codec) {
   }
 
   // If the codec is available, set it as the default in m line.
-  var payload = getCodecPayloadType(sdpLines, codec);
-  if (payload) {
-    sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], payload);
+  var payload = null;
+  for (var i = 0; i < sdpLines.length; i++) {
+   var index = findLineInRange(sdpLines, i, -1, 'a=rtpmap', codec);
+   if(index !== null) {
+     payload = getCodecPayloadTypeFromLine(sdpLines[index])
+     if (payload) {
+       sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], payload);
+     }
+   }
   }
 
   sdp = sdpLines.join('\r\n');
