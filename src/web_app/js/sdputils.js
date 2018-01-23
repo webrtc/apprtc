f161c7153da8bdff5b6b20ad63f6dfdca367c250
Tyler Gibson <tyler.gibson@microsoft.com>
2018-01-22 20:05:22 -0800

    Fixing linting

--- src/web_app/js/sdputils.js
+++ src/web_app/js/sdputils.js
@@ -332,13 +332,13 @@ function maybePreferCodec(sdp, type, dir, codec) {
   // If the codec is available, set it as the default in m line.
   var payload = null;
   for (var i = 0; i < sdpLines.length; i++) {
-   var index = findLineInRange(sdpLines, i, -1, 'a=rtpmap', codec);
-   if(index !== null) {
-     payload = getCodecPayloadTypeFromLine(sdpLines[index])
-     if (payload) {
-       sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], payload);
-     }
-   }
+    var index = findLineInRange(sdpLines, i, -1, 'a=rtpmap', codec);
+    if (index !== null) {
+      payload = getCodecPayloadTypeFromLine(sdpLines[index]);
+      if (payload) {
+        sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], payload);
+      }
+    }
   }
 
   sdp = sdpLines.join('\r\n');
