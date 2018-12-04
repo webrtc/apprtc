7794bae24aa0914dbf1f6aa03204445cfada163f
Anton Khayrudinov <ankh@google.com>
2018-12-04 14:01:51 -0800

    Removed _onBodyClick from webrtc.js

--- src/web_app/js/webrtc.js
+++ src/web_app/js/webrtc.js
@@ -32,101 +32,87 @@ class WebRTC {
       Module.onRuntimeInitialized = () => {
         console.warn('webrtc.wasm loaded');
         console.log('wasm module:', Module);
-        const Transport = Module.Transport.extend("Transport", {
-          __construct: function() {
-            this.__parent.__construct.call(this);
-          },
-          __destruct: function() {
-            this.__parent.__destruct.call(this);
-          },
-          sendPacket: function(payload) {
-            console.log('sendPacket');
-            console.log(payload);
-            let payloadCopy = new Module.VectorUint8();
-            for (let i = 0; i < payload.length; i++) {
-              payloadCopy.push_back(payload[i]);
-            }
-            call.deliverPacket(payloadCopy);
-            return true;
-          },
-        });
-        var call = new Module.Call(new Transport());
-        let sendStream = call.createAudioSendStream({
-          ssrc: 123,
-          cname: 'cname',
-          payloadType: 42,
-          codecName: 'opus',
-          clockrateHz: 48000,
-          numChannels: 2,
-        });
-        sendStream.start();
-
-        function sendSomeAudio(offset) {
-          let sendBuffer = new Module.VectorInt16();
-          for (let i = 0; i < 480 * 2; i++) {
-            sendBuffer.push_back(offset + i);
-          }
-          console.log('sending audio');
-          sendStream.sendAudioData({
-            data: sendBuffer,
-            numChannels: 2,
-            sampleRateHz: 48000,
-            samplesPerChannel: 480,
-            timestamp: 0,
-          });
-          setTimeout(() => sendSomeAudio((offset + 10) % 100 - 50), 1000);
-        }
-        setTimeout(() => sendSomeAudio(0), 1000);
-
-        const AudioSink =
-            Module.AudioReceiveStreamSink.extend("AudioReceiveStreamSink", {
-              __construct: function() {
-                this.__parent.__construct.call(this);
-              },
-              __destruct: function() {
-                this.__parent.__destruct.call(this);
-              },
-              onAudioFrame: function(audioFrame) {
-                console.log('onAudioFrame');
-                console.log(audioFrame);
-              },
-            });
-
-        let receiveAudioCodecs = new Module.VectorAudioCodec();
-        receiveAudioCodecs.push_back({
-          payloadType: 42,
-          name: 'opus',
-          clockrateHz: 48000,
-          numChannels: 2,
-        });
-        let receiveStream = call.createAudioReceiveStream({
-          localSsrc: 345,
-          remoteSsrc: 123,
-          codecs: receiveAudioCodecs,
-        });
-        receiveStream.setSink(new AudioSink());
-        receiveStream.start();
+        this._onWasmLoaded();
       };
     };
 
     document.body.appendChild(script);
   }
 
-  _onBodyClick() {
-    console.warn('initializing libvpx');
+  _onWasmLoaded() {
+    const Transport = Module.Transport.extend("Transport", {
+      __construct: function () {
+        this.__parent.__construct.call(this);
+      },
+      __destruct: function () {
+        this.__parent.__destruct.call(this);
+      },
+      sendPacket: function (payload) {
+        console.log('sendPacket');
+        console.log(payload);
+        let payloadCopy = new Module.VectorUint8();
+        for (let i = 0; i < payload.length; i++) {
+          payloadCopy.push_back(payload[i]);
+        }
+        call.deliverPacket(payloadCopy);
+        return true;
+      },
+    });
+    var call = new Module.Call(new Transport());
+    let sendStream = call.createAudioSendStream({
+      ssrc: 123,
+      cname: 'cname',
+      payloadType: 42,
+      codecName: 'opus',
+      clockrateHz: 48000,
+      numChannels: 2,
+    });
+    sendStream.start();
 
-    const VP8 = 0x30385056;
-    const width = 640;
-    const height = 480;
-    const yuvdata = new Uint8Array(width * height * 3 / 2);
+    function sendSomeAudio(offset) {
+      let sendBuffer = new Module.VectorInt16();
+      for (let i = 0; i < 480 * 2; i++) {
+        sendBuffer.push_back(offset + i);
+      }
+      console.log('sending audio');
+      sendStream.sendAudioData({
+        data: sendBuffer,
+        numChannels: 2,
+        sampleRateHz: 48000,
+        samplesPerChannel: 480,
+        timestamp: 0,
+      });
+      setTimeout(() => sendSomeAudio((offset + 10) % 100 - 50), 1000);
+    }
+    setTimeout(() => sendSomeAudio(0), 1000);
 
-    FS.writeFile('/vpx-yuv', yuvdata); // in-memory memfs emscripten file
+    const AudioSink =
+      Module.AudioReceiveStreamSink.extend("AudioReceiveStreamSink", {
+        __construct: function () {
+          this.__parent.__construct.call(this);
+        },
+        __destruct: function () {
+          this.__parent.__destruct.call(this);
+        },
+        onAudioFrame: function (audioFrame) {
+          console.log('onAudioFrame');
+          console.log(audioFrame);
+        },
+      });
 
-    _vpx_js_encoder_init(VP8, width, height);
-    _vpx_js_encoder_process();
-    _vpx_js_encoder_exit(); // flushes all memory buffers, etc.
-
-    const ivfdata = FS.readFile('/vpx-ivf');
-    console.log('IVF data:', ivfdata);
-  }
+    let receiveAudioCodecs = new Module.VectorAudioCodec();
+    receiveAudioCodecs.push_back({
+      payloadType: 42,
+      name: 'opus',
+      clockrateHz: 48000,
+      numChannels: 2,
+    });
+    let receiveStream = call.createAudioReceiveStream({
+      localSsrc: 345,
+      remoteSsrc: 123,
+      codecs: receiveAudioCodecs,
+    });
+    receiveStream.setSink(new AudioSink());
+    receiveStream.start();
+  };
 }
