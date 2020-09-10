0f92cab741ae8172857dcffb85e2b6f5da83769d
Doudou Kisabaka <648530+kisabaka@users.noreply.github.com>
2020-09-10 19:40:56 +0200

    Remove remaining artefacts from the Chrome app build which we don't use anymore.

--- src/web_app/js/storage.js
+++ src/web_app/js/storage.js
@@ -9,14 +9,12 @@
 /* More information about these options at jshint.com/docs/options */
 
 /* exported Storage */
-/* globals chrome */
 
 'use strict';
 
 var Storage = function() {};
 
 // Get a value from local browser storage. Calls callback with value.
-// Handles variation in API between localStorage and Chrome app storage.
 Storage.prototype.getStorage = function(key, callback) {
   // Use localStorage.
   var value = localStorage.getItem(key);
@@ -28,7 +26,6 @@ Storage.prototype.getStorage = function(key, callback) {
 };
 
 // Set a value in local browser storage. Calls callback after completion.
-// Handles variation in API between localStorage and Chrome app storage.
 Storage.prototype.setStorage = function(key, value, callback) {
   // Use localStorage.
   localStorage.setItem(key, value);
